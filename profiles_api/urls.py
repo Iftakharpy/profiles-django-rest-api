cfe94cce3ae1601ae96464070c375569ef18192a
Iftakhar Ahmed <iftakharn19@gmail.com>
2021-01-19 19:53:39 +0600

    add viewset to profiles_api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import HelloWorldAPIView
+from django.urls import path, include
+from .views import HelloWorldAPIView, HelloViewSet, json_response
+from rest_framework.routers import DefaultRouter
+
+#retister the HelloViewSet
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
-    path('hello/', HelloWorldAPIView.as_view()),
+    path('json/<int:count>/', json_response),
+    path('hello-view/', HelloWorldAPIView.as_view()),
+    path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from .serializers import HelloSerializer
 
@@ -40,3 +42,52 @@ class HelloWorldAPIView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = HelloSerializer
+    
+    def list(self, request):
+        """Return a list with even number from 1 to 99. And a "Hello" message."""
+        response_list = [x for x in range(1,99) if x%2==0]
+        return Response({'messge': "Hello World!", 'even_numbers': response_list})
+
+    def create(self, request):
+        """Create a hello message."""
+        serialized_data = self.serializer_class(data=request.data)
+
+        if serialized_data.is_valid():
+            name = serialized_data.validated_data.get('name')
+            message = f"Hello, {name}!"
+            return Response({'message': message, 'method': request.method})
+        return Response(
+            serialized_data.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
+    
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by it's ID."""
+        return Response({'http_method': "GET", 'method': request.method})
+    
+    def update(self, request, pk=None):
+        """Handle updating an object by it's ID."""
+        return Response({'http_method': 'PUT', 'method': request.method})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating the provided fields with only the provided fields by it's ID."""
+        return Response({'http_method': 'PATCH', 'method': request.method})
+    
+    def destroy(self, request, pk=None):
+        """Handle deleting an object by it's ID."""
+        return Response({'http_method': "DELETE", 'method': request.method})
+
+
+def json_response(request, count):
+    # print(request.user)
+    
+    a = [x for x in range(count) if x%10==0]
+    resp = {'logged_in': request.user.is_authenticated, 'status': 'success', 'nums': a}
+    if request.user.is_authenticated:
+        return HttpResponse(json.dumps(resp))
+    return HttpResponse('Error 404: Not found.')
